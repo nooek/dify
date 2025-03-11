0ac813c7d92babdc53d5768b7faa77eb895dea98
ivespf12 <Ives@koalitytech.com>
2025-03-11 18:19:39 -0300

    Reverting changes after test

--- web/app/activate/page.tsx
+++ web/app/activate/page.tsx
@@ -22,7 +22,7 @@ const Activate = () => {
         <Header />
         <ActivateForm />
         <div className='px-8 py-6 text-sm font-normal text-gray-500'>
-          © {new Date().getFullYear()} Change, Inc. All rights reserved.
+          © {new Date().getFullYear()} Koality, Inc. All rights reserved.
         </div>
       </div>
     </div>
